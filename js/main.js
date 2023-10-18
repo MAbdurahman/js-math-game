3dd4387e173e0bf5b7ebc08428f2b99b98dfa781
MAbdurahman <mdbdrrhm@gmail.com>
2023-10-18 09:20:15 -0500

    Add preloader for project; complete user interface for game and score sections

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
+/* ============================================
+            preloader
+===============================================*/
+$(window).on('load', function () {
+    // makes sure that whole site is loaded
+    $('#preloader-gif, #preloader').fadeOut(5000, function () {});
+});
+
+/*=============================================
+         js-math-game scripts
+================================================*/
+
 $(function() {
+    console.log('js-math-game scripts');
     console.log('The DOM is ready to be used.');
+
+
 
 })
